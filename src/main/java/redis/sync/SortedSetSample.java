57268c25dfa17147e9f4a402d8caf2c55f872b4f
LG10033 <eomsh@line.games>
2020-09-03 16:43:45 +0900

    sortedset 관련 수정

--- src/main/java/redis/sync/SortedSetSample.java
+++ src/main/java/redis/sync/SortedSetSample.java
@@ -20,13 +20,16 @@ public class SortedSetSample {
 		final String key = "rank";
 
 		if (syncCommands.exists(key) > 0) {
-			syncCommands.del("rank"); //테스트 데이터 저장 전에 삭제 진행
+			syncCommands.del(key); //테스트 데이터 저장 전에 삭제 진행
 		}
 
 		//성능을 생각하면 가능하면 하나의 컬렉션에는 1만개 이하의 아이템을 담는게 좋음
 		for (int i = 1; i <= 100; i++) {
 			syncCommands.zadd(key, i, String.format("user-%s", i));
 		}
+
+		syncCommands.zincrby(key, 100, "user-1");
+		System.out.println("갯수:" + syncCommands.zcard(key));
 
 		System.out.println(syncCommands.zrange(key, 1, 2));
 
