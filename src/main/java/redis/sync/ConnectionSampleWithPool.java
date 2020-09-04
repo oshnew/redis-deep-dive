c2259993abcccc3dd6aa60339454cd3e06c45ec7
LG10033 <eomsh@line.games>
2020-09-04 18:40:04 +0900

    커넥션풀 샘플소스 추가

--- src/main/java/redis/sync/ConnectionSampleWithPool.java
+++ src/main/java/redis/sync/ConnectionSampleWithPool.java
@@ -10,32 +10,49 @@ import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
 import io.lettuce.core.support.ConnectionPoolSupport;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-import redis.common.RedisClientConfig;
 
 /**
+ * redis 커넥션풀 기반 연결 샘플
  *
  * @author 엄승하
  */
 public class ConnectionSampleWithPool {
 
-	public static void main(String[] args) throws Exception {
+	private static final String REDIS_CON_URL = "redis://192.168.56.1:6379/0";
+	private static GenericObjectPool<StatefulRedisConnection<String, String>> pool = null; //sync & 비 클러스터모드 커넥션풀
 
-		GenericObjectPool<StatefulRedisConnection<String, String>> pool = nonClusterPoolUsage();
+	public static void main(String[] args) throws InterruptedException {
 
-		try (StatefulRedisConnection<String, String> connection = pool.borrowObject()) {
-			connection.sync().set("key-pool-test", String.valueOf(System.currentTimeMillis()));
-			String value = connection.sync().get("key-pool-test");
-			System.out.println(value);
-		} catch (RedisConnectionException e) {
-			System.out.println(String.format("Failed to connect to Redis server: %s", e));
+		pool = nonClusterPoolUsage();
+		//pool = useClusterPoolUsage(); //클러스터모드일때
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		testSetValue("key-pool-test-1", String.valueOf(System.currentTimeMillis()));
+		testSetValue("key-pool-test-2", String.valueOf(System.currentTimeMillis()));
+		testSetValue("key-pool-test-3", String.valueOf(System.currentTimeMillis()));
+
+		Thread.sleep(10 * 1000); //커넥션풀 close 전에 대기 후, redis-cli(서버)에서 client list 명령어로 현재 연결된 클라이언트 리스트를 확인해보면 됨
 
 		pool.close();
 		System.out.println("finish");
+	}
 
+	/**
+	 * set value 테스트
+	 *
+	 * @param key
+	 * @param value
+	 */
+	private static void testSetValue(String key, String value) {
+
+		try (StatefulRedisConnection<String, String> connection = pool.borrowObject()) { //pool을 이용해서 커맨드 실행
+			connection.sync().set(key, value);
+			value = connection.sync().get(key);
+			System.out.println(value);
+		} catch (RedisConnectionException e) {
+			System.out.println(String.format("Failed to connect to Redis server: %s", e));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -45,7 +62,7 @@ public class ConnectionSampleWithPool {
 	 */
 	private static GenericObjectPool<StatefulRedisConnection<String, String>> nonClusterPoolUsage() {
 
-		RedisClient client = RedisClient.create(RedisClientConfig.REDIS_URI);
+		RedisClient client = RedisClient.create(REDIS_CON_URL);
 		client.setOptions(ClientOptions.builder().autoReconnect(true).build());
 
 		return ConnectionPoolSupport.createGenericObjectPool(() -> client.connect(), createPoolConfig());
@@ -59,7 +76,7 @@ public class ConnectionSampleWithPool {
 	 */
 	private static GenericObjectPool<StatefulRedisClusterConnection<String, String>> useClusterPoolUsage() {
 
-		RedisClusterClient clusterClient = RedisClusterClient.create(RedisClientConfig.REDIS_URI);
+		RedisClusterClient clusterClient = RedisClusterClient.create(REDIS_CON_URL);
 		clusterClient.setOptions(ClusterClientOptions.builder().autoReconnect(true).build());
 
 		return ConnectionPoolSupport.createGenericObjectPool(() -> clusterClient.connect(), createPoolConfig());
