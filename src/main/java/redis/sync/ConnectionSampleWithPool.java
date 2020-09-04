a721931f34d00d799f1e7b1e15d7d31e46a2a725
LG10033 <eomsh@line.games>
2020-09-04 19:47:17 +0900

    커넥션풀 샘플소스 추가: 동시성 테스트

--- src/main/java/redis/sync/ConnectionSampleWithPool.java
+++ src/main/java/redis/sync/ConnectionSampleWithPool.java
@@ -11,6 +11,12 @@ import io.lettuce.core.support.ConnectionPoolSupport;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * redis 커넥션풀 기반 연결 샘플
  *
@@ -18,17 +24,40 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
  */
 public class ConnectionSampleWithPool {
 
-	private static final String REDIS_CON_URL = "redis://192.168.56.1:6379/0";
-	private static GenericObjectPool<StatefulRedisConnection<String, String>> pool = null; //sync & 비 클러스터모드 커넥션풀
+	private static final String REDIS_CON_URL = "redis://192.168.56.1:6379/0"; //로컬 redis 0번 사용
+	public static GenericObjectPool<StatefulRedisConnection<String, String>> pool = null; //sync & 비 클러스터모드 커넥션풀
 
-	public static void main(String[] args) throws InterruptedException {
+	private static AtomicInteger counter = new AtomicInteger(0);
+
+	public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
 
 		pool = nonClusterPoolUsage();
 		//pool = useClusterPoolUsage(); //클러스터모드일때
 
-		testSetValue("key-pool-test-1", String.valueOf(System.currentTimeMillis()));
-		testSetValue("key-pool-test-2", String.valueOf(System.currentTimeMillis()));
-		testSetValue("key-pool-test-3", String.valueOf(System.currentTimeMillis()));
+		int nThreads = 10;
+		ExecutorService es = Executors.newFixedThreadPool(nThreads);
+
+		CyclicBarrier barrier = new CyclicBarrier(nThreads + 1); //동시 테스트를 위해서 각각의 스레드를 대기처리할 목적
+		for (int i = 0; i < nThreads; i++) {
+
+			//System.out.println(String.format("for문의 i:'%s'", i));
+
+			es.submit(() -> {
+				int idx = counter.addAndGet(1);
+				System.out.println(String.format("idx:'%s'", idx));
+
+				barrier.await(); //대기: cyclicBarrier 를 생성할 때, 인자값으로 준 count 개수만큼 // await를 호출한다면 모든 쓰레드의 wait 상태가 종료
+				System.out.println(String.format("getNumberWaiting: '%s'", barrier.getNumberWaiting()));
+				System.out.println(String.format("Thread idx: %s", idx));
+
+				testSetValue(String.valueOf("test-thread-" + idx), String.valueOf(idx));
+				return null;
+			});
+		}
+
+		//		testSetValue("key-pool-test-1", "key-pool-test-1");
+		//		testSetValue("key-pool-test-2", "key-pool-test-2");
+		//		testSetValue("key-pool-test-3", "key-pool-test-3");
 
 		Thread.sleep(10 * 1000); //커넥션풀 close 전에 대기 후, redis-cli(서버)에서 client list 명령어로 현재 연결된 클라이언트 리스트를 확인해보면 됨
 
@@ -46,8 +75,8 @@ public class ConnectionSampleWithPool {
 
 		try (StatefulRedisConnection<String, String> connection = pool.borrowObject()) { //pool을 이용해서 커맨드 실행
 			connection.sync().set(key, value);
-			value = connection.sync().get(key);
-			System.out.println(value);
+			String rsltValue = connection.sync().get(key);
+			System.out.println(String.format("get key's value '%s' -> '%s'", key, rsltValue));
 		} catch (RedisConnectionException e) {
 			System.out.println(String.format("Failed to connect to Redis server: %s", e));
 		} catch (Exception e) {
@@ -91,14 +120,14 @@ public class ConnectionSampleWithPool {
 
 		GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
 
-		poolConfig.setMaxTotal(20);
-		poolConfig.setMaxIdle(10);
+		poolConfig.setMaxTotal(200);
+		poolConfig.setMaxIdle(100);
 
 		// "true" will result better behavior when unexpected load hits in production
 		// "false" makes it easier to debug when your maxTotal/minIdle/etc settings need adjusting.
 		poolConfig.setBlockWhenExhausted(true);
 		poolConfig.setMaxWaitMillis(1000);
-		poolConfig.setMinIdle(5);
+		poolConfig.setMinIdle(100);
 
 		return poolConfig;
 	}
